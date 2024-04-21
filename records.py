94bc84f758f2c3a6303bdcd323c3d62b1f1c877e
Omar Altahat <33757266+Aleovas@users.noreply.github.com>
2024-04-21 10:08:56 +0300

    Adding documentation and refactoring to allow for more valid system configurations

--- records.py
+++ records.py
@@ -8,6 +8,7 @@ import re
 import openpyxl
 import shutil
 import csv
+import os
 from datetime import date
 from docx import Document
 from docx.enum.table import WD_TABLE_ALIGNMENT
@@ -17,6 +18,12 @@ from docx.shared import Pt, Cm, Inches
 # Round Order toggle. Set to false to revert to alphabetical room order for list sorting
 # When set to true, list order starts with the higher floors and works its way down, with list order for 3B/4B reversed
 ROUND_ORDER = True
+
+# Vista font size selection
+FONT_SIZE = "12"
+
+# Get current logged in windows user. This is used as a workaround for user-only installs of tesseract
+USER = os.getlogin()
 
 # This command hides the window for the program when after it starts to not cover the Vista window
 ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 6 )
@@ -53,6 +60,7 @@ def roomClear(x):
     x=x.strip("-")
     x=x.replace("5LD-TX","BLD-TX")
     x=x.replace("DAYCAS","DAYCASE")
+    x=x.replace("DC-KSBi","DC-KSB")
     return x
 
 # Workaround for common OCR errors when reading patient's MRN
@@ -65,7 +73,7 @@ def mrnClear(x):
     x=x.replace("D","5")
     if not x or x.isspace(): return ""
     if x[0]=="0": x="5"+x
-
+    x=x.strip("!")
     return x
 
 # Given a patient, this function returns the floor the patient is on
@@ -118,26 +126,37 @@ class Patient:
             if(not self.room[0:2]=="10" and obj.room[0:2]=="10"): return True
             return ((self.room) < (obj.room))
  
-# Initialization for pyautogui (the input automation library) an pytesseract (the OCR library) 
+# Initialization for pyautogui (the input automation library) and pytesseract (the OCR library) 
 pyautogui.FAILSAFE = True
-pytesseract.pytesseract.tesseract_cmd=r'C:\Users\Oa.16675\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
-cancelTop=pyautogui.locateOnScreen("cancel.png").top
-cancelLeft=pyautogui.locateOnScreen("cancel.png").left
+pytesseract.pytesseract.tesseract_cmd=fr'C:\Users\{USER}\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+
+# Using screenshots to find the "Cancel" and "Specialties" buttons
+cancelTop=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\cancel.png").top
+cancelLeft=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\cancel.png").left
 try:
-    spec=pyautogui.locateOnScreen("spec.png")
+    spec=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\spec.png")
     pyautogui.click(spec.left,spec.top)
 except:
     print("Failed to find specialties button")
+
+# Defining a dictionary containing the relavent teams, a screenshot showing how they appear in Vista
+# and initializing their floor counts at zero    
 teams={
-    "Team 1": ([],"team1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Team 2": ([],"team2.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Team 3": ([],"team3.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Team 5": ([],"team5.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Lymphoma": ([],"teamLymphoma.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Leukemia": ([],"teamLeukemia.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Palliative": ([],"teampal1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0})
+    "Team 1": ([],fr"Data\{FONT_SIZE}\team1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
+    "Team 2": ([],fr"Data\{FONT_SIZE}\team2.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
+    "Team 3": ([],fr"Data\{FONT_SIZE}\team3.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
+    "Team 4": ([],fr"Data\{FONT_SIZE}\team4.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
+    "Team 5": ([],fr"Data\{FONT_SIZE}\team5.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
+    "Lymphoma": ([],fr"Data\{FONT_SIZE}\teamLymphoma.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
+    "Leukemia": ([],fr"Data\{FONT_SIZE}\teamLeukemia.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
+    "Palliative": ([],fr"Data\{FONT_SIZE}\teampal1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0})
 }
+
+# Program pauses for one second to ensure that the specialties list loaded. 
+# It then tries to find each team. If not found it tries to scroll down automatically
+# If patient is still ultimately not found it tells the user to manually locate it
 time.sleep(1)
+scrollbox=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\patientScroll.png") # The scroll bar for patient list
 for team in teams.items():
     found = False
     while not found:
@@ -148,53 +167,65 @@ for team in teams.items():
             found=True
         except:
             try:
-                box=pyautogui.locateOnScreen("scroll.png")
+                box=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\scroll.png")
                 pyautogui.click(box.left,box.top+20)
                 time.sleep(1)
             except:
                 pyautogui.alert(text=f'Program failed to scroll automatically to find {team[0]}, please scroll manually until it is visible and click OK', title='Scroll failed', button='OK')
-    pyautogui.click(464,134)
+    # Clicks on the first patient in the list. Location is relative to the scroll bar 
+    pyautogui.click(scrollbox.left-300,scrollbox.top+5)
     pal=False
+    # This assumes a maximum of 40 patients per team
     for i in range(40):
         time.sleep(0.8)
-        nameImage=pyautogui.screenshot(r'C:\Users\Oa.16675\Downloads\python\1.png', region=(cancelLeft-409,cancelTop+45,392,20))
-        roomImage=ImageEnhance.Contrast(pyautogui.screenshot(r'C:\Users\Oa.16675\Downloads\python\3.png', region=(cancelLeft-195,cancelTop+152,74,36))).enhance(20)
-        mrnImage=ImageEnhance.Contrast(pyautogui.screenshot(r'C:\Users\Oa.16675\Downloads\python\2.png', region=(cancelLeft,cancelTop+80,68,27))).enhance(20)
+        # Takes screenshots of name, MRN, and room and uses tesseract 
+        nameImage=pyautogui.screenshot(r'1.png', region=(cancelLeft-409,cancelTop+45,392,20))
+        roomImage=ImageEnhance.Contrast(pyautogui.screenshot(r'3.png', region=(cancelLeft-195,cancelTop+152,74,36))).enhance(20)
+        mrnImage=ImageEnhance.Contrast(pyautogui.screenshot(r'2.png', region=(cancelLeft,cancelTop+80,68,27))).enhance(20)
         name=pytesseract.image_to_string(nameImage).strip().replace(".",",").replace("_",",").replace("|","").strip("\'\"|/\\").strip("\'\"|/\\")
         if not name or name.isspace(): 
+            # Moving to next list if it reaches an empty name (i.e. end of list)
+            # Below is special code to account for palliative being split into two lists 
             print("break")
             if pal: break
-            if(team[1][1]=="teampal1.png"):
-                box=pyautogui.locateOnScreen("teampal2.png")
+            if(team[1][1]==fr"Data\{FONT_SIZE}\teampal1.png"):
+                box=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\teampal2.png")
                 pyautogui.click(box.left,box.top)
                 time.sleep(1)
-                pyautogui.click(464,134)
+                pyautogui.click(scrollbox.left-300,scrollbox.top+5)
                 pal=True
                 continue
             break
-        mrn=mrnClear(pytesseract.image_to_string(mrnImage, config="--psm 7"))
-        
+        mrn=mrnClear(pytesseract.image_to_string(mrnImage, config="--psm 7"))        
         roomstr=pytesseract.image_to_string(roomImage, lang="eng", config="--psm 7").strip().strip("\'\"|/\\,.").strip("\'\"|/\\").strip()
+        # Code for skipping fake patients
         if not mrn or mrn.isspace() or "Test" in name or "Pacs" in name or not roomstr or len(roomstr)<=2:
             pyautogui.press('down')
             continue
         room=roomClear(roomstr)
+        # Creating a new patient with the collected information, 
+        # then pressing the down button to get next patient
         team[1][0].append(Patient(name,room,mrn))
-        team[1][0].sort()
         pyautogui.press('down')
     print(str(team[0]))
 
+# Copies the template excel file to new file named after today's date
+# Then it gets the team counts sheet and finds the relavent teams
 excelName= f'{date.today()}.xlsx'
 shutil.copy("template.xlsx",excelName)
 wb = openpyxl.load_workbook(excelName)
 counts = wb.get_sheet_by_name('Counts')
 teamNameCells=tuple(counts["A2":"A8"])
+
+# Getting the override list 
 override={}
 with open('override.csv') as overrideCSV:
     csv_reader = csv.DictReader(overrideCSV)
     for row in csv_reader:
         override[row["mrn"]]=row["team"]
-        
+
+# Moving patients in override list, and removing patients in certain floors
+ignoredFloors=['2B','4H','5H','6H']        
 for team in teams.items():
     remove=[]
     for patient in team[1][0]:
@@ -202,24 +233,26 @@ for team in teams.items():
         if patient.mrn in override.keys() and not override[patient.mrn]==team[0]:
             if not override[patient.mrn]=="NA": teams[override[patient.mrn]][0].append(patient)
             remove.append(patient)
-        
-        if patient.room[0:2]=='2B':
-            remove.append(patient)
-        if patient.room[0:2]=='4H':
-            remove.append(patient)
-        if patient.room[0:2]=='5H':
+        if patient.room[0:2] in ignoredFloors:
             remove.append(patient)
     for patient in remove:
         team[1][0].remove(patient)
-        
+
+# Counting patients, updating team counts, and adding them to team list in excel        
 for team in teams.items():
     team[1][0].sort()
     for patient in team[1][0]:
+        # If patient's room isn't in floors 2-5, adds them to the towers count 
+        # which acts as a catch-all for other patiens (Daycase, IV unit, etc.)
         pattern=re.compile(r'[2-5][ABC]')
         if(pattern.match(patient.room[0:2])): team[1][2][patient.room[0:2]]+=1
         else: team[1][2]["Towers"]+=1
     print(f'{team[0]}: {team[1][2]}')
-
+    # When it gets to team 4, it ignores it and goes to other teams
+    # Team 4 doesn't need to have a list
+    if team[0]=="Team 4": continue
+    
+    # Updates team count
     for cell in teamNameCells:
         if cell[0].value == team[0]:
             r=cell[0].row
@@ -228,6 +261,7 @@ for team in teams.items():
                 counts.cell(row=r,column=c).value=count
                 c+=1
 
+    # Gets sheet for team list. Adds patients to each team's list
     teamSheet=wb.get_sheet_by_name(team[0])
     r=2
     for patient in team[1][0]:
@@ -236,8 +270,8 @@ for team in teams.items():
         teamSheet.cell(row=r,column=3).value=patient.room
         teamSheet.cell(row=r,column=4).value=patient.mrn
         r+=1
-
-wb.save(excelName)
+# Saves the excel file. If an error occurs (Usually because excel file is already open)
+# it prompts the user to close it
 while True:
     try:
         wb.save(excelName)
@@ -245,6 +279,7 @@ while True:
     except:
         pyautogui.alert(text='Please close Excel file and press OK', title='File write error', button='OK')
 
+# Creates new word document and sets some style formatting
 document = Document()
 style = document.styles['No Spacing']
 font = style.font
@@ -254,7 +289,11 @@ font.bold = True
 normalstyle = document.styles['Normal']
 normalstyle.font.name = 'Calibri'
 
+# Creates list for each team in the word document
 for team in teams.items():
+    # When it gets to team 4, it ignores it and goes to other teams
+    # Team 4 doesn't need to have a list
+    if team[0]=="Team 4": continue
     title=document.add_paragraph(f'{team[0]} ({date.today()})')
     title.alignment = WD_ALIGN_PARAGRAPH.CENTER
     title.style = style
@@ -280,6 +319,8 @@ for team in teams.items():
         i+=1
     if not team[0] == "Palliative": document.add_page_break()   
 
+# Saves the word file. If an error occurs (Usually because word file is already open)
+# it prompts the user to close it
 while True:
     try:
         document.save(f"{date.today()}.docx")
