c09a242041eb634f766da40ac556b826176342d4
Omar Altahat <33757266+Aleovas@users.noreply.github.com>
2024-04-29 00:18:40 +0300

    Added comparision to previous excel file, if present, for more team stats

--- records.py
+++ records.py
@@ -10,10 +10,14 @@ import shutil
 import csv
 import os
 from datetime import date
+from datetime import timedelta
 from docx import Document
 from docx.enum.table import WD_TABLE_ALIGNMENT
 from docx.enum.text import WD_ALIGN_PARAGRAPH
 from docx.shared import Pt, Cm, Inches
+from docx.oxml.ns import nsdecls
+from docx.oxml import parse_xml
+
 
 # Round Order toggle. Set to false to revert to alphabetical room order for list sorting
 # When set to true, list order starts with the higher floors and works its way down, with list order for 3B/4B reversed
@@ -28,8 +32,12 @@ USER = os.getlogin()
 # This command hides the window for the program when after it starts to not cover the Vista window
 ctypes.windll.user32.ShowWindow( ctypes.windll.kernel32.GetConsoleWindow(), 6 )
 
+# Whether yesterday's excel file is present or not
+YESTERDAY_PRESENT = False
+
 # Workaround for common OCR errors when reading patient's rooms
 def roomClear(x):
+    print(x)
     x=x.replace("2-","2A-")
     x=x.replace("28-","2B-")
     x=x.replace("A0","A-0")
@@ -63,13 +71,19 @@ def roomClear(x):
     x=x.replace("DC-KSBi","DC-KSB")
     x=x.replace("3-","3A-")
     x=x.replace("4-","4A-")
+    x=x.replace("4A4A","4A")
+    x=x.replace("40-","4C-")
     x=x.replace("5-","5C-")
+    x=x.replace("QH","9H")
+    x=x.replace("A2","A-2")
+    print(x)
     return x
 
 # Workaround for common OCR errors when reading patient's MRN
 # Known error: Sometimes the number '5' is read as '6'. This issue is unavoidable with current implementation.
 #              Possible fixes require image manipulation which will increase time for list generation significantly
 def mrnClear(x):
+    print(x)
     x=x.strip().strip("\'\"|/\\").strip("\'\"|/\\‘°").replace(" ","")
     x=x.replace("i","1")
     x=x.replace("o","9")
@@ -80,6 +94,7 @@ def mrnClear(x):
     x=x.strip("!")
     if x[0]=="T": x="1"+x[1:]
     if len(x)==7: x="2"+x[2:]
+    print(x)
     return x
 
 # Given a patient, this function returns the floor the patient is on
@@ -105,6 +120,7 @@ class Patient:
         self.name = name.replace(",,",",")
         self.room = room
         self.mrn = mrn
+        self.new = False
     
     # The "less than" comparator definition for this class. Python requires defining at least one comparision method for sorting objects.
     # This function works by defining what the "<" operator returns  (i.e. when patient a (self)< patient b (obj) is true).
@@ -150,16 +166,18 @@ except:
     print("Failed to find specialties button")
 
 # Defining a dictionary containing the relavent teams, a screenshot showing how they appear in Vista
-# and initializing their floor counts at zero    
+# and initializing their floor counts at zero. Also stores two integers representing new patients to 
+# team and patients no longer there, respectively.
+# It
 teams={
-    "Team 1": ([],fr"Data\{FONT_SIZE}\team1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Team 2": ([],fr"Data\{FONT_SIZE}\team2.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Team 3": ([],fr"Data\{FONT_SIZE}\team3.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Team 4": ([],fr"Data\{FONT_SIZE}\team4.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Team 5": ([],fr"Data\{FONT_SIZE}\team5.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Lymphoma": ([],fr"Data\{FONT_SIZE}\teamLymphoma.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Leukemia": ([],fr"Data\{FONT_SIZE}\teamLeukemia.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0}),
-    "Palliative": ([],fr"Data\{FONT_SIZE}\teampal1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0})
+    "Team 1": ([],fr"Data\{FONT_SIZE}\team1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0]),
+    "Team 2": ([],fr"Data\{FONT_SIZE}\team2.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0]),
+    "Team 3": ([],fr"Data\{FONT_SIZE}\team3.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0]),
+    "Team 4": ([],fr"Data\{FONT_SIZE}\team4.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0]),
+    "Team 5": ([],fr"Data\{FONT_SIZE}\team5.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0]),
+    "Lymphoma": ([],fr"Data\{FONT_SIZE}\teamLymphoma.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0]),
+    "Leukemia": ([],fr"Data\{FONT_SIZE}\teamLeukemia.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0]),
+    "Palliative": ([],fr"Data\{FONT_SIZE}\teampal1.png",{"2A":0,"3A":0,"3B":0,"3C":0,"4A":0,"4B":0,"4C":0,"5C":0,"Towers":0},[0,0])
 }
 
 # Program pauses for one second to ensure that the specialties list loaded. 
@@ -172,9 +190,11 @@ cancel=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\cancel.png")
 searchregion=(cancel.left-600,cancel.top,700,400)
 for team in teams.items():
     found = False
-    while not found:
+    # Tries to look for the team being counted based on the screenshot defined in the team dictionary
+    # If it fails it attempts scrolling down to find team, if it still fails it will prompt the user to 
+    # find it manually
+    while not found: 
         try:
-            print(team[1][1])
             box=pyautogui.locateOnScreen(team[1][1])
             pyautogui.click(box.left,box.top)
             found=True
@@ -186,7 +206,7 @@ for team in teams.items():
             except:
                 pyautogui.alert(text=f'Program failed to scroll automatically to find {team[0]}, please scroll manually until it is visible and click OK', title='Scroll failed', button='OK')
     # Clicks on the first patient in the list. Location is relative to the scroll bar 
-    pyautogui.click(scrollbox.left-300,scrollbox.top+5)
+    pyautogui.click(scrollbox.left-250,scrollbox.top+5)
     pal=False
     time.sleep(0.8)
     # Finds region for screenshot of patient data in vista
@@ -196,8 +216,8 @@ for team in teams.items():
     roomRegion=(khcc.left,khcc.top+normalize(20),normalize(75),normalize(20))
     mrn=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\mrn.png", region=searchregion)
     mrnRegion=(mrn.left+normalize(45),mrn.top,normalize(65),normalize(20))
-    # This assumes a maximum of 40 patients per team
-    for i in range(4):
+    # This assumes a maximum of 45 patients per team
+    for i in range(45):
         time.sleep(0.8)
         # Takes screenshots of name, MRN, and room and uses tesseract for OCR
         nameImage=pyautogui.screenshot(r'1.png', region=nameRegion)
@@ -207,7 +227,6 @@ for team in teams.items():
         if not name or name.isspace(): 
             # Moving to next list if it reaches an empty name (i.e. end of list)
             # Below is special code to account for palliative being split into two lists 
-            print("break")
             if pal: break
             if(team[1][1]==fr"Data\{FONT_SIZE}\teampal1.png"):
                 box=pyautogui.locateOnScreen(fr"Data\{FONT_SIZE}\teampal2.png")
@@ -230,10 +249,51 @@ for team in teams.items():
         pyautogui.press('down')
     print(str(team[0]))
 
+# Array storing MRNs for patients in teams based on yesterday's excel files
+yesterdayTeams={
+    "Team 1":[],
+    "Team 2":[],
+    "Team 3":[],
+    "Team 5":[],
+    "Lymphoma":[],
+    "Leukemia":[],
+    "Palliative":[]
+}
+
+# If excel file is present for yesterday, it reads it for extra statistics
+yesterdayExcel=f'{date.today()-timedelta(days = 1)}.xlsx'
+if os.path.isfile(yesterdayExcel):
+    wb = openpyxl.load_workbook(yesterdayExcel)
+    YESTERDAY_PRESENT = True
+    for team in yesterdayTeams.items():
+        teamSheet=wb.get_sheet_by_name(team[0])
+        r=2
+        while True:
+            mrn=teamSheet.cell(row=r,column=4).value
+            if not mrn or mrn.isspace(): break
+            team[1].append(teamSheet.cell(row=r,column=4).value)           
+            r+=1
+            
+    for team in teams.items():
+        if team[0]=="Team 4": continue
+        for patient in team[1][0]:
+            if patient.mrn not in yesterdayTeams[team[0]]:
+                patient.new=True
+                team[1][3][0] += 1 # Updates new patient counter for the team
+        mrnsToday=[x.mrn for x in team[1][0]]
+        for patient in yesterdayTeams[team[0]]: # Updates missing patient counter
+            if patient not in mrnsToday: team[1][3][1] += 1
+        print(f'New/Old: {team[1][3][0]}/{team[1][3][1]}')
+
 # Copies the template excel file to new file named after today's date
 # Then it gets the team counts sheet and finds the relavent teams
 excelName= f'{date.today()}.xlsx'
-shutil.copy("template.xlsx",excelName)
+while True:
+    try:
+        shutil.copy("template.xlsx",excelName)
+        break
+    except:
+        pyautogui.alert(text='Please close Excel file and press OK', title='File write error', button='OK')
 wb = openpyxl.load_workbook(excelName)
 counts = wb.get_sheet_by_name('Counts')
 teamNameCells=tuple(counts["A2":"A8"])
@@ -258,10 +318,10 @@ for team in teams.items():
             remove.append(patient)
     for patient in remove:
         team[1][0].remove(patient)
-
+    team[1][0].sort()
+    
 # Counting patients, updating team counts, and adding them to team list in excel        
 for team in teams.items():
-    team[1][0].sort()
     for patient in team[1][0]:
         # If patient's room isn't in floors 2-5, adds them to the towers count 
         # which acts as a catch-all for other patiens (Daycase, IV unit, etc.)
@@ -281,6 +341,8 @@ for team in teams.items():
             for count in team[1][2].values():
                 counts.cell(row=r,column=c).value=count
                 c+=1
+            counts.cell(row=r,column=12).value=team[1][3][0]
+            counts.cell(row=r,column=13).value=team[1][3][1]
 
     # Gets sheet for team list. Adds patients to each team's list
     teamSheet=wb.get_sheet_by_name(team[0])
@@ -299,7 +361,7 @@ while True:
         break
     except:
         pyautogui.alert(text='Please close Excel file and press OK', title='File write error', button='OK')
-
+        
 # Creates new word document and sets some style formatting
 document = Document()
 style = document.styles['No Spacing']
@@ -319,7 +381,7 @@ for team in teams.items():
     title.alignment = WD_ALIGN_PARAGRAPH.CENTER
     title.style = style
     row_number=len(team[1][0])+1
-    row_height=int(Inches(8.5)/row_number)
+    row_height=int(Inches(8.4)/row_number)
     table=document.add_table(rows=row_number, cols=4)
     table.alignment = WD_TABLE_ALIGNMENT.CENTER
     table.style = 'Table Grid'
@@ -333,12 +395,17 @@ for team in teams.items():
         table.rows[i].cells[3].width=0
         table.rows[i].cells[2].text=patient.room
         table.rows[i].cells[2].width=0
-        table.rows[i].cells[1].text=patient.name
+        table.rows[i].cells[1].text=patient.name 
         table.rows[i].cells[1].width=Cm(50)
-        table.rows[i].cells[0].text=str(i)
+        table.rows[i].cells[0].text=str(i) +("*" if patient.new else "")
         table.rows[i].cells[0].width=0
+        # Adds shading if its a new patient
+        if patient.new: 
+            for cell in table.rows[i].cells:
+                shading_elm = parse_xml(r'<w:shd {} w:fill="D9D9D9"/>'.format(nsdecls('w')))
+                cell._tc.get_or_add_tcPr().append(shading_elm)
         i+=1
-    if not team[0] == "Palliative": document.add_page_break()   
+    if team[0]!="Palliative": document.add_page_break()   
 
 # Saves the word file. If an error occurs (Usually because word file is already open)
 # it prompts the user to close it
